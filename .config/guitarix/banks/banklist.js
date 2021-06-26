4f75941c87eb8e877de8def6de17c39b2fe3485c
iamthem <lslopez@andrew.cmu.edu>
2021-06-26 10:20:35 -0500

    Added guitarix config files, deepin config files and pip dependencies for deep-python environment

---
+++ .config/guitarix/banks/banklist.js
@@ -0,0 +1,8 @@
+[
+  ["arizona", "arizona.gx", 1, 0, [1, 2], 1621129404],
+  ["cleansounds", "cleansounds.gx", 1, 0, [1, 2], 1621129271],
+  ["Chaos", "Chaos.gx", 1, 0, [1, 2], 1617837979],
+  ["metalpack-v1", "metalpack-v1.gx", 1, 0, [1, 2], 1617837916],
+  ["hk-1", "hk-1.gx", 1, 0, [1, 2], 1618534436],
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1622914148]
+]
